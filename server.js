bde81a7cffa8cc7d3a87c8cc89202b242d9b5f46
ujjawalhub <ujjawal2604@gmail.com>
2017-08-08 13:35:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/ui/article-one', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/ui/article-two', function (req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+app.get('/ui/article-three', function (req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
