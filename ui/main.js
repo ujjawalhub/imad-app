1fe83a0ded5e977c139cc669eb7a8bf7ede54f1c
ujjawalhub <ujjawal2604@gmail.com>
2017-08-09 17:17:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 var img = document.getElementById('qwe');
-var marginLeft = 0;
+var marginLeft ;
 function moveRight (){
     do {  marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
