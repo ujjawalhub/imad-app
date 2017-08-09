fe1ca4c9b2ee745bbe3d72abdbfca002bbaa5d10
ujjawalhub <ujjawal2604@gmail.com>
2017-08-09 15:55:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/web234.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'web234.png'));
 });
