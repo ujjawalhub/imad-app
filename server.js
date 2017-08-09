96b4c65f3643db1a78b89201fe92b163e4320777
ujjawalhub <ujjawal2604@gmail.com>
2017-08-09 13:02:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ function createTemp (data){
 var template = `
         <html>
         <head>
-         <link href="/ui/style.css" rel="stylesheet" />
              <title> ${title}
             </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
