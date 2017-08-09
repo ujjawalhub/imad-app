2757c4549721a86a2283f599711e3b1b42a6df6a
ujjawalhub <ujjawal2604@gmail.com>
2017-08-09 16:35:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var marginLeft = 0;
 var img = document.getElementById('qwe');
 function moveRight (){
-    var marginLeft = marginLeft + 100;
+    var marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function (){
-    var interval = setInterval(moveRight , 100);
+    var interval = setInterval(moveRight , 50);
 };
