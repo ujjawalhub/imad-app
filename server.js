27f6142e46a73a6fb6cf979331a2480fa54a7b2a
ujjawalhub <ujjawal2604@gmail.com>
2017-08-10 13:20:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ var counter = 0;
 app.get('/counter', function (req, res){
   counter = counter + 1 ;
   res.send(counter.toString());
+  return counter;
 });
 
 app.get('/:articleName', function (req, res){
