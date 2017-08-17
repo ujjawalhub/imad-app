51b7067bcb40562d9879f4fef899bd3a57b59999
ujjawalhub <ujjawal2604@gmail.com>
2017-08-17 15:12:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,20 @@ button.onclick = function(){
           }
       }  
     };
-    
    req.open('GEt' , 'http://ujjawal2604.imad.hasura-app.io/counter' , true);
    req.send(null);
-    
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_bt')
+submit.onclick = function (){
+    var names= ['name1', 'name2', 'name3', 'name4']
+    var list = '';
+    for(var i= 0; i < names.length; i++){
+        list += '<li>' + names + '</li>';
+         }
+     var ul = document.getElementById('namelist');
+     ul.innerHTML = list;
+     
+}
