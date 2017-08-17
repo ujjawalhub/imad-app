07ec9cbb42efad8984b672736a936af2c03ce807
ujjawalhub <ujjawal2604@gmail.com>
2017-08-17 15:16:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,10 +32,10 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_bt');
 submit.onclick = function (){
-    var names= ['name1', 'name2', 'name3', 'name4'];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for(var i= 0; i < names.length; i++){
-        list += '<li>' + names + '</li>';
+        list += '<li>' + names[i] + '</li>';
          }
      var ul = document.getElementById('namelist');
      ul.innerHTML = list;
